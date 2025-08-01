25506890f32c510e1bce61f1daac90d0cde9ad2d
Chetankumargithub <146056386+Chetankumargithub@users.noreply.github.com>
2025-08-01 11:19:27 +0530

    Update and rename Subtraction to Subtraction.cpp

---
+++ Subtraction.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a , b, c;
+cout<<"enter first numbber" ;
+cin>>a;
+cout<<"Enter second number ";
+cin>>b;
+c=a-b;
+cout<<"The answer is "<<c;
+return 0;
+}
